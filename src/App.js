2374453431dee1b5135dceba378af6e7997d173c
amitpalsingh1712@gmail.com <amitpalsingh1712@gmail.com>
2022-03-16 12:54:48 +0530

    netflix clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// 0d24939e3e593d09f03e8eaba6c08349
+
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./request";
+import "./Row.css";
+import Banner from "./Banner";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Banner />
+      <Row
+        title="Netflix Originals "
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row
+        title="NetFlix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row
+        className="row_style"
+        title="Romance Movies"
+        fetchUrl={requests.fetchRomanceMovies}
+      />
+      <Row
+        className="row_style"
+        title="Documantaries Movies"
+        fetchUrl={requests.fetchDocumantaries}
+      />
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Navbar.css";
+import { Icon } from "@iconify/react";
+
+function Navbar() {
+  return (
+    <div className={`nav`}>
+      <Icon icon="logos:netflix" className="nav_logo" />
+      <div className="nav_buttons">
+        <button className="nav_button_login">Login</button>
+        <button className="nav_button_signup">Sign In</button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
